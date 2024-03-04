8a3f62baee531d76dde088a3743fa3133a5662f3
Claudia Olavarrieta <claudiaomtnez@gmail.com>
2024-03-04 09:48:24 -0500

    Sidebar styles and icons

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,10 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
+import LogoS from '../../assets/images/logo-c.png';
+import LogoSubtitle from '../../assets/images/sub-logo-c.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -24,6 +25,21 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} />
             </NavLink>
         </nav>
+
+        <ul>
+            <li>
+                <a target='_blank' rel='noreferrer' 
+                href='https://linkedin.com/in/claudia-olavarrieta-mart%C3%ADnez-33b8541aa'>
+                    <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                </a>
+            </li>
+            <li>
+                <a target='_blank' rel='noreferrer' 
+                href='https://github.com/ClaudiaOM'>
+                    <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
